c8df3878b68350fb837075d5d7fd2da2f80064ef
OSINT <polaramkatariya50@gmail.com>
2026-01-11 21:37:10 +0530

    Update apis.py

--- apis.py
+++ apis.py
@@ -11,38 +11,59 @@ def safe_get(url, params=None):
 def api_num(n):
     return safe_get(
         "https://subhxcosmo-osint-api.onrender.com/api",
-        {"key":"VNIOX","type":"mobile","term":n}
+        {"key": "VNIOX", "type": "mobile", "term": n}
     )
 
 def api_vehicle_num(v):
     return safe_get(
         "https://subhxcosmo-osint-api.onrender.com/api",
-        {"key":"VNIOX","type":"vehicle_num","term":v}
+        {"key": "VNIOX", "type": "vehicle_num", "term": v}
+    )
+
+def api_vehicle_info(rc):
+    return safe_get(
+        "https://vnioxcyber.vercel.app/api/vehicle",
+        {"rc": rc}
     )
 
 def api_id_family(i):
     return safe_get(
         "https://subhxcosmo-osint-api.onrender.com/api",
-        {"key":"VNIOX","type":"id_family","term":i}
+        {"key": "VNIOX", "type": "id_family", "term": i}
     )
-
-def api_rc(rc):
-    return safe_get("https://vnioxcyber.vercel.app/api/vehicle", {"rc":rc})
 
 def api_pk(n):
     return safe_get(
         "https://paknum.amorinthz.workers.dev/",
-        {"key":"AMORINTH","number":n}
+        {"key": "AMORINTH", "number": n}
+    )
+
+def api_rc(rc):
+    return safe_get(
+        "https://vnioxcyber.vercel.app/api/vehicle",
+        {"rc": rc}
     )
 
 def api_ff(uid):
-    return safe_get("https://api-cr-ffinfo.kesug.com/ff.php", {"uid":uid})
+    return safe_get(
+        "https://api-cr-ffinfo.kesug.com/ff.php",
+        {"uid": uid}
+    )
 
 def api_ifsc(code):
-    return safe_get("https://ab-ifscinfoapi.vercel.app/info", {"ifsc":code})
+    return safe_get(
+        "https://ab-ifscinfoapi.vercel.app/info",
+        {"ifsc": code}
+    )
 
 def api_calltrace(n):
-    return safe_get("https://ab-calltraceapi.vercel.app/info", {"number":n})
+    return safe_get(
+        "https://ab-calltraceapi.vercel.app/info",
+        {"number": n}
+    )
 
 def api_fampay(fid):
-    return safe_get("https://fampay-2-number.vercel.app/get-number", {"id":fid})
+    return safe_get(
+        "https://fampay-2-number.vercel.app/get-number",
+        {"id": fid}
+    )
