d552e8a0e6c972ec390d4b3435b4347930a069bd
OSINT <polaramkatariya50@gmail.com>
2026-01-11 21:19:19 +0530

    Create db.py

---
+++ db.py
@@ -0,0 +1,12 @@
+from pymongo import MongoClient
+
+class Database:
+    def __init__(self, uri, name):
+        self.client = MongoClient(uri)
+        self.db = self.client[name]
+
+    def add_user(self, uid):
+        self.db.users.update_one({"_id": uid}, {"$set": {"_id": uid}}, upsert=True)
+
+    def get_users(self):
+        return [u["_id"] for u in self.db.users.find({}, {"_id": 1})]
