5426ef5f0d5897d65a5df4f12a1b5724ec383c2f
OSINT <polaramkatariya50@gmail.com>
2026-01-11 21:38:49 +0530

    Update db.py

--- db.py
+++ db.py
@@ -6,7 +6,11 @@ class Database:
         self.db = self.client[name]
 
     def add_user(self, uid):
-        self.db.users.update_one({"_id": uid}, {"$set": {"_id": uid}}, upsert=True)
+        self.db.users.update_one(
+            {"_id": uid},
+            {"$set": {"_id": uid}},
+            upsert=True
+        )
 
     def get_users(self):
         return [u["_id"] for u in self.db.users.find({}, {"_id": 1})]
