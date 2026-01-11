e43c7dc03c6eac9602c5222149cf501f2aaef485
OSINT <polaramkatariya50@gmail.com>
2026-01-11 21:36:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,6 @@ BOT_TOKEN = "PASTE_BOT_TOKEN"
 
 OWNER_IDS = {6972508083}
 
-ALLOWED_GROUP_ID = -1002401784535
-ALLOWED_GROUP_LINK = "https://t.me/OsintInformationGroup"
-
 MUST_JOIN_CHANNELS = {
     -1001596819852: "https://t.me/+whz_G-xn0KdkZWI1",
     -1003389483500: "https://t.me/heroku_club",
@@ -17,17 +14,16 @@ CLONE_FREE_DAYS = 3
 CLONE_PRICE_INR = 400   # per month
 
 # Logs
-LOG_CHANNEL_ID = -100XXXXXXXXXX   # private channel/group
+LOG_CHANNEL_ID = -100XXXXXXXXXX   # private log channel/group
 
 # Mongo
 MONGO_URI = "PASTE_MONGO_URI"
 DB_NAME = "vni0x"
 
 # Image system
-THEME = "dark"          # "dark" or "light"
+THEME = "dark"          # dark / light
 ENABLE_BLUR = True
 ENABLE_WATERMARK = True
 CACHE_ENABLED = True
 
-# WATERMARK (UPDATED AS REQUESTED)
 WATERMARK_TEXT = "VNIOX • OSINT"
