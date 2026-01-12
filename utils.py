392863a5247c9f443fe1f67e8c495516bcc5b940
OSINT <polaramkatariya50@gmail.com>
2026-01-12 12:08:32 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import uuid
 
-def text_to_txt_file(text, name="result"):
+def save_txt(text, name="result"):
     path = f"/tmp/{name}_{uuid.uuid4().hex[:6]}.txt"
     with open(path, "w", encoding="utf-8") as f:
         f.write(text)
