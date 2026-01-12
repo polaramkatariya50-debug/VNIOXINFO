a3aa48cc4de3b18eec1d2b187ca4b5cead5c309a
OSINT <polaramkatariya50@gmail.com>
2026-01-12 12:07:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,22 +1,23 @@
-import os
+# ===== TELEGRAM BOT =====
+BOT_TOKEN = "8556191807:AAGt7noEjSaRlxZChZPFCorb3DlaxcTO_X0"
 
-BOT_TOKEN = os.getenv("8556191807:AAGt7noEjSaRlxZChZPFCorb3DlaxcTO_X0")
-
+# ===== OWNER =====
 OWNER_IDS = {6972508083}
 
-MONGO_URI = os.getenv("mongodb+srv://vishalpandeynkp:Bal6Y6FZeQeoAoqV@cluster0.dzgwt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+# ===== DATABASE =====
+MONGO_URI = "mongodb+srv://vishalpandeynkp:Bal6Y6FZeQeoAoqV@cluster0.dzgwt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DB_NAME = "vni0x"
 
-# MUST JOIN CHANNELS (5)
-MUST_JOIN_CHANNELS = {
-    1: "https://t.me/+whz_G-xn0KdkZWI1",
-    2: "https://t.me/heroku_club",
-    3: "https://t.me/NOBITA_SUPPORT",
-    4: "https://t.me/OsintInformationGroup",
-    5: "https://t.me/+Ah7RcBKx4zQ2YTE1",
-}
+# ===== MUST JOIN CHANNELS (5) =====
+MUST_JOIN_CHANNELS = [
+    "https://t.me/+whz_G-xn0KdkZWI1",
+    "https://t.me/heroku_club",
+    "https://t.me/NOBITA_SUPPORT",
+    "https://t.me/OsintInformationGroup",
+    "https://t.me/+Ah7RcBKx4zQ2YTE1",
+]
 
-# 🔴 BLOCKED GROUP (BOT COMPLETELY SILENT)
+# ===== BLOCKED GROUP (BOT SILENT) =====
 BLOCKED_GROUP_IDS = {
     -1002363071054
 }
