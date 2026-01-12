2d936a2b51dc646ba6331f626a7878cb43808e28
OSINT <polaramkatariya50@gmail.com>
2026-01-12 10:50:05 +0530

    Update pdf_exporter.py

--- pdf_exporter.py
+++ pdf_exporter.py
@@ -1,6 +1,6 @@
 from PIL import Image
 
 def image_to_pdf(img_path):
-    pdf_path = img_path.replace(".png", ".pdf")
-    Image.open(img_path).convert("RGB").save(pdf_path)
-    return pdf_path
+    pdf = img_path.replace(".png", ".pdf")
+    Image.open(img_path).convert("RGB").save(pdf, "PDF", resolution=300.0)
+    return pdf
