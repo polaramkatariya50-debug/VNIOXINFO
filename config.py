e2e998f02f125cf9164ae1dcd573bcae48c53b99
OSINT <polaramkatariya50@gmail.com>
2026-01-12 12:20:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,10 @@
-# ===== TELEGRAM BOT =====
 BOT_TOKEN = "8556191807:AAGt7noEjSaRlxZChZPFCorb3DlaxcTO_X0"
 
-# ===== OWNER =====
 OWNER_IDS = {6972508083}
 
-# ===== DATABASE =====
 MONGO_URI = "mongodb+srv://vishalpandeynkp:Bal6Y6FZeQeoAoqV@cluster0.dzgwt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DB_NAME = "vni0x"
 
-# ===== MUST JOIN CHANNELS (5) =====
 MUST_JOIN_CHANNELS = [
     "https://t.me/+whz_G-xn0KdkZWI1",
     "https://t.me/heroku_club",
@@ -17,7 +13,6 @@ MUST_JOIN_CHANNELS = [
     "https://t.me/+Ah7RcBKx4zQ2YTE1",
 ]
 
-# ===== BLOCKED GROUP (BOT COMPLETELY SILENT) =====
 BLOCKED_GROUP_IDS = {
     -1002363071054
 }
