222f9e4361a635a0676f3acd427821a04f2afc69
OSINT <polaramkatariya50@gmail.com>
2026-01-12 12:13:00 +0530

    Update apis.py

--- apis.py
+++ apis.py
@@ -1,39 +1,42 @@
 import requests
 
-def fetch(url, params):
+TIMEOUT = 40
+
+def get(url, params=None):
     try:
-        r = requests.get(url, params=params, timeout=40)
+        r = requests.get(url, params=params, timeout=TIMEOUT)
+        r.raise_for_status()
         return r.json()
     except:
         return {}
 
 def api_india_number(n):
-    return fetch("https://subhxcosmo-osint-api.onrender.com/api",
-                 {"key":"VNIOX","type":"mobile","term":n})
-
-def api_vehicle_info(v):
-    return fetch("https://vnioxcyber.vercel.app/api/vehicle", {"rc":v})
+    return get("https://subhxcosmo-osint-api.onrender.com/api",
+               {"key":"VNIOX","type":"mobile","term":n})
 
 def api_vehicle_num(v):
-    return fetch("https://subhxcosmo-osint-api.onrender.com/api",
-                 {"key":"VNIOX","type":"vehicle_num","term":v})
+    return get("https://subhxcosmo-osint-api.onrender.com/api",
+               {"key":"VNIOX","type":"vehicle_num","term":v})
 
-def api_pak_number(n):
-    return fetch("https://paknum.amorinthz.workers.dev/",
-                 {"key":"AMORINTH","number":n})
-
-def api_ff(uid):
-    return fetch("https://api-cr-ffinfo.kesug.com/ff.php", {"uid":uid})
-
-def api_ifsc(code):
-    return fetch("https://ab-ifscinfoapi.vercel.app/info", {"ifsc":code})
-
-def api_calltrace(n):
-    return fetch("https://ab-calltraceapi.vercel.app/info", {"number":n})
-
-def api_fampay(fid):
-    return fetch("https://fampay-2-number.vercel.app/get-number", {"id":fid})
+def api_vehicle_info(rc):
+    return get("https://vnioxcyber.vercel.app/api/vehicle", {"rc":rc})
 
 def api_id_family(a):
-    return fetch("https://subhxcosmo-osint-api.onrender.com/api",
-                 {"key":"VNIOX","type":"id_family","term":a})
+    return get("https://subhxcosmo-osint-api.onrender.com/api",
+               {"key":"VNIOX","type":"id_family","term":a})
+
+def api_pak_number(n):
+    return get("https://paknum.amorinthz.workers.dev/",
+               {"key":"AMORINTH","number":n})
+
+def api_ff(uid):
+    return get("https://api-cr-ffinfo.kesug.com/ff.php", {"uid":uid})
+
+def api_ifsc(code):
+    return get("https://ab-ifscinfoapi.vercel.app/info", {"ifsc":code})
+
+def api_calltrace(n):
+    return get("https://ab-calltraceapi.vercel.app/info", {"number":n})
+
+def api_fampay(fid):
+    return get("https://fampay-2-number.vercel.app/get-number", {"id":fid})
